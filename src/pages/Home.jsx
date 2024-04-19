2ab9f2a5e8444b0d2598882d19d496d4684c7730
Lakshitha Eranga <sanjueranga16@gmail.com>
2024-04-19 10:23:09 +0530

    feat: home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Container, Typography } from '@mui/material';
+
+const Home = () => {
+  return (
+    <Container>
+      <Typography variant="h4" align="center">Welcome to the Funerals App</Typography>
+      {/* Add more content as needed */}
+    </Container>
+  );
+}
+
+export default Home;
