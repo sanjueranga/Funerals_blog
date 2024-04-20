de3a641269e097ef949d9714449a7641067a5659
Lakshitha Eranga <sanjueranga16@gmail.com>
2024-04-20 11:35:21 +0530

    WIP

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -5,21 +5,21 @@ import Grid from "@mui/material/Grid";
 const Post = ({ title, publishedAt, body }) => {
   return (
     <Grid item xs={8}>
+      <Typography
+        sx={{
+          fontSize: "24px",
+          fontFamily: "lato",
+          fontWeight:"900"
+        }}
+      >
+        {title || "How to Deal with Loss in the Workplace"}
+      </Typography>{" "}
+      <Typography variant="body2" color="text.secondary">
+        {publishedAt || "Published at: Unknown"}{" "}
+      </Typography>
       <Paper sx={{ padding: (theme) => theme.spacing(2) }}>
         {" "}
         <Stack spacing={1}>
-          <Typography
-            variant="h3"
-            sx={{
-              fontSize: "24px",
-              fontFamily: "lato",
-            }}
-          >
-            {title || "How to Deal with Loss in the Workplace"}
-          </Typography>{" "}
-          <Typography variant="body2" color="text.secondary">
-            {publishedAt || "Published at: Unknown"}{" "}
-          </Typography>
           <Typography variant="body1">{body}</Typography>
         </Stack>
       </Paper>
